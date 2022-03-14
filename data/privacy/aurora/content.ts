52cec119a6faf7ee18dfb7a973227dacb37b14ab
Rafael Nepomuceno <rafaelnepomuceno@live.com>
2022-03-14 12:40:36 -0300

    Página de política de privacidade para Aurora

---
+++ data/privacy/aurora/content.ts
@@ -0,0 +1,63 @@
+export default {
+  title: 'Aurora Privacy Policy',
+  author: {
+    name: 'Nunlup',
+    picture: 'https://lh3.googleusercontent.com/ogw/ADea4I5JrzcJZy25o3MrutSQBLXFnfNsFMHbvqTguXQ1=s32-c-mo',
+  },
+  date: '2022-03-14',
+  ogImage: {
+    url: 'https://lh3.googleusercontent.com/QKv4hgi5XJmzo6Zxe-jDA3US5_BmsYb5PEDPXB1K0pc-j2QkKS50VSKiwr-ONpcjc_4'
+  },
+  coverImage: 'https://images.unsplash.com/photo-1503945438517-f65904a52ce6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+  content: `
+    <p>
+    We regard the protection and the confidentiality of your data as very important. In this Privacy Policy, we would like to tell you which personal data we collect and what we use it for.
+    Below, we'll tells you about the type and extent of and the purposes for which personal data are collected and used. You can call up this information on our website anytime.
+    </p>
+    Subject of Data Protection
+    The subject of data protection is personal data. These include details such as your name, your postal address, your email address, your telephone number or, where appropriate, even use-data.
+    </p>
+    <p>
+    <h2>Collection and Use of Data</h2></br>
+    <strong>Automated Data Collection</strong></br>
+    When you access our websites, your internet browser automatically transmits data for technical reasons. The following data are stored separately from other data that you may have sent us:
+    your date and time of access
+    your browser-type and version
+    the operating system you use
+    the URL of the website you have previously visited
+    the quantity of data transmitted
+    For technical reasons these data are stored and are not assigned to any identifiable person at any time.
+    </p>
+    <p>
+    <strong>Data Entered by Users</strong></br>
+    You can use our apps both with and without registering. However, you can only access our apps using certain terminals such as an iPhone, iPod, iPad, Windows, Mac or Android. In order to make use of all its advantages, you have to register. To do this, you must enter your email address and select a password. We require these data in order to guarantee that you are able to make full use of our entire offer. We need these and possibly other data not least in order to respond to your wishes, questions and criticism.
+    The central function of our apps is task management. You will be able to use this product both online and offline.
+    Where our apps are used solely offline, the data you enter will be stored de-centrally on your storage medium. In such a case, we will not be able to access these data.
+    If you use our apps in combination with a registered account, all data and tasks entered will be integrated into a database. The purpose of this database is to allow you to use our apps at various different levels and call up your tasks anywhere and at any time. Data are transmitted to the respective terminal using SSL encryption so that your data are transmitted securely.
+    </p>
+    <p>
+    <strong>Transfer of Data</strong></br>
+    We will not transfer your data to third parties as a matter of course without letting you know in advance or asking for your prior permission. We may only transfer your data to third parties without informing you separately beforehand in the following exceptional cases as explained below:
+    If required for investigating the illegal use of our apps or for legal proceedings, personal data will be transferred to the criminal investigation authorities and, if appropriate, to injured third parties. We will only do this if there are concrete indications of illegal and/or abusive behavior. We can only transfer on your personal data if this is used to enforce General Terms and Conditions of Business or other agreements. We are also legally obliged to give certain public authorities information.
+    Occasionally we depend on contractually affiliated external companies and external service providers to supply services such as the supply of advertising measures (only if you have given your explicit prior consent), processing payments (PayPal, credit card etc.), storing your data and customer service. In such cases, information is transferred to these companies or individuals in order to enable them to process this information further. We carefully select these external service providers and review them regularly to ensure that your privacy is preserved. The service providers may only use the data for the purposes stipulated by us.
+    </p>
+    <p>
+    <strong>Cookies</strong></br>
+    our application stores so-called "cookies" in order to be able to offer you a comprehensive range of functions and to make it easier to use our websites. "Cookies" are small files which are stored on your computer with the help of the internet browser. If you do not want to use "cookies", you can prevent "cookies" from being stored on your computer using the corresponding settings on your internet browser. Please note that this may restrict the functional capability and the range of functions of our offer.
+    We use cookies and similar technologies for several purposes, including:
+    Storing your Preferences and Settings. Settings that enable our websites to operate correctly or that maintain your preferences over time may be stored on your device.
+    Sign-in and Authentication. When you sign into a website, we store a unique ID number, and the time you signed in, in an encrypted cookie on your device. This cookie allows you to move from page to page within the site without having to sign in again on each page.
+    Security. We use cookies to detect fraud and abuse of our websites.
+    Analytics. In order to provide our products, we use cookies and other identifiers to gather usage and performance data. For example, we use cookies to count the number of unique visitors to a web page or service and to develop other statistics about the operations of our products.
+    Performance. our application uses cookies for load balancing to ensure that our websites remain up and running.
+    </p>
+    <p>
+    <strong>Deleting Your Data</strong></br>
+    If your data are no longer required for the aforementioned purposes, we will delete them. if you want to delete all your data, please contact us by email: aurora@nunlup.com.
+
+    <p>
+    <strong>Amendments to this Privacy Policy</strong></br>
+    We reserve the right to amend this Privacy Policy. You can call up the current version of the Privacy Policy at any time at: https://nunlup.com/privacy/aurora.
+    </p>
+  `
+};
---
+++ pages/privacy/aurora.tsx
@@ -0,0 +1,54 @@
+import { useRouter } from 'next/router'
+import ErrorPage from 'next/error'
+import Container from '../../components/container'
+import PostBody from '../../components/post-body'
+import Header from '../../components/header'
+import PostHeader from '../../components/post-header'
+import Layout from '../../components/layout'
+import { getPostBySlug, getAllPosts } from '../../lib/api'
+import PostTitle from '../../components/post-title'
+import Head from 'next/head'
+import { CMS_NAME } from '../../lib/constants'
+import markdownToHtml from '../../lib/markdownToHtml'
+import PostType from '../../types/post'
+import post from '../../data/privacy/aurora/content';
+
+type Props = {
+  post: PostType
+  morePosts: PostType[]
+  preview?: boolean
+}
+
+const Post = ({ morePosts, preview }: Props) => {
+  const router = useRouter()
+  return (
+    <Layout preview={preview}>
+      <Container>
+        <Header />
+        {router.isFallback ? (
+          <PostTitle>Loading…</PostTitle>
+        ) : (
+          <>
+            <article className="mb-32">
+              <Head>
+                <title>
+                  {post.title} | Next.js Blog Example with {CMS_NAME}
+                </title>
+                <meta property="og:image" content={post.ogImage.url} />
+              </Head>
+              <PostHeader
+                title={post.title}
+                coverImage={post.coverImage}
+                date={post.date}
+                author={post.author}
+              />
+              <PostBody content={post.content} />
+            </article>
+          </>
+        )}
+      </Container>
+    </Layout>
+  )
+}
+
+export default Post
